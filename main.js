7ba7d7a32c4e68dc82020239538730276406aeb0
Esmatullah <101590645+Esmatullah12@users.noreply.github.com>
2023-04-26 06:21:43 +0100

    Update the main.js file
    refactor the code main.js
    - Use JavaScript classes instead of objects and arrays. 
    - Create class methods to add and remove books.

--- main.js
+++ main.js
@@ -1,59 +1,63 @@
-/* eslint-disable  */
-const addBookForm = document.getElementById('add-book-form');
-const booksList = document.getElementById('books-list');
-
-let books = JSON.parse(localStorage.getItem('books')) || [];
-
-// display the books list 
-books.forEach((book) => {
-  const bookElement = createBookElement(book.title, book.author);
-  booksList.appendChild(bookElement);
-});
-
-addBookForm.addEventListener('submit', (event) => {
-  event.preventDefault();
-
-  // Get the input values
-  const titleInput = document.getElementById('title');
-  const authorInput = document.getElementById('author');
-  const title = titleInput.value;
-  const author = authorInput.value;
-
-  // Add the new book
-  const newBook = { title, author };
-  books.push(newBook);
-
-  // Save the updated books array
-  localStorage.setItem('books', JSON.stringify(books));
-
-  const bookElement = createBookElement(title, author);
-  booksList.appendChild(bookElement);
-
-  // Reset the form
-  addBookForm.reset();
-});
-
-// function to create a book element
-function createBookElement(title, author) {
-  const bookApp = document.createElement('div');
-  bookApp.classList.add('book');
-  bookApp.innerHTML = `
-        <p>${title}</p>
-        <p>${author}</p>
-        <button class="remove-btn">Remove</button>
-        <hr>
-    `;
-
-  const removeButton = bookApp.querySelector('.remove-btn');
-  removeButton.addEventListener('click', () => {
-    // Remove the book from the books array
-    books = books.filter((book) => book.title !== title || book.author !== author);
-
-    // Save the updated books array to local storage
-    localStorage.setItem('books', JSON.stringify(books));
-
-    bookApp.remove();
-  });
-
-  return bookApp;
-}
+class Book {
+    constructor(title, author) {
+      this.title = title;
+      this.author = author;
+    }
+  }
+  
+  class Library {
+    constructor() {
+      this.books = JSON.parse(localStorage.getItem('books')) || [];
+      this.addBookForm = document.getElementById('add-book-form');
+      this.booksList = document.getElementById('books-list');
+      this.addBookForm.addEventListener('submit', this.addBook.bind(this));
+      this.render();
+    }
+  
+    render() {
+      this.booksList.innerHTML = '';
+      this.books.forEach(book => {
+        const bookElement = this.createBookElement(book);
+        this.booksList.appendChild(bookElement);
+      });
+    }
+  
+    addBook(event) {
+      event.preventDefault(); 
+      const titleInput = document.getElementById('title');
+      const authorInput = document.getElementById('author');
+      const title = titleInput.value;
+      const author = authorInput.value;
+      const newBook = new Book(title, author);
+      this.books.push(newBook);
+      localStorage.setItem('books', JSON.stringify(this.books));
+      const bookElement = this.createBookElement(newBook);
+      this.booksList.appendChild(bookElement);
+      this.addBookForm.reset();
+    }
+  
+    removeBook(title, author) {
+      this.books = this.books.filter(book => book.title !== title || book.author !== author);
+      localStorage.setItem('books', JSON.stringify(this.books));
+      this.render();
+    }
+  
+    createBookElement(book) {
+      const bookApp = document.createElement('div');
+      bookApp.classList.add('book');
+      bookApp.innerHTML = `
+          <p>${book.title}</p>
+          <p>${book.author}</p>
+          <button class="remove-btn">Remove</button>
+          <hr>
+      `;
+      const removeButton = bookApp.querySelector('.remove-btn');
+      removeButton.addEventListener('click', () => {
+          this.removeBook(book.title, book.author);
+          bookApp.remove();
+      });
+      return bookApp;
+    }
+  }
+  
+  const library = new Library();  
