fccbd8ddb74641029890dfaedf6bf216dc50a64a
wolterbwambale <benplaan1@gmail.com>
2023-04-28 02:24:13 +0300

    add navigation

--- main.js
+++ main.js
@@ -51,5 +51,34 @@ class Library {
     return bookApp;
   }
 }
+
 const myLibrary = new Library();
+
+function showSection(sectionId) {
+  const sections = document.querySelectorAll('main section');
+
+  sections.forEach((section) => {
+    if (section.id === sectionId) {
+      section.classList.add('active');
+    } else {
+      section.classList.remove('active');
+    }
+  });
+}
+
+const datetimeElement = document.getElementById('datetime');
+const currentDate = new Date();
+const formattedDate = currentDate.toLocaleString();
+datetimeElement.innerHTML = ` ${formattedDate}`;
+
+function handleLinkClick(e) {
+  e.preventDefault();
+  showSection(e.target.dataset.section);
+}
+
+document.querySelectorAll('nav a').forEach((link) => {
+  link.addEventListener('click', handleLinkClick);
+});
+
+showSection('home');
 myLibrary.books();
